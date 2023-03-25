80424e2e3d13ccdecfbb089fe20a6def3772bbe8
Oleksandr Makogin <sashamakohin@gmail.com>
2023-03-25 13:19:08 +0200

    .

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,68 @@
-export const getTasks = state => state.tasks.items;
+import { statusFilters } from './constants';
+import { createSelector } from '@reduxjs/toolkit';
 
-export const getStatusFilter = state => state.filters.status;
+export const selectTasks = state => state.tasks.items;
+
+export const selectIsLoading = state => state.tasks.isLoading;
+
+export const selectError = state => state.tasks.error;
+
+export const selectStatusFilter = state => state.filters.status;
+
+// export const selectVisibleTasks = state => {
+//   const tasks = selectTasks(state);
+//   const statusFilter = selectStatusFilter(state);
+
+//   switch (statusFilter) {
+//     case statusFilters.active:
+//       return tasks.filter(task => !task.completed);
+//     case statusFilters.completed:
+//       return tasks.filter(task => task.completed);
+//     default:
+//       return tasks;
+//   }
+// };
+
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
+  }
+);
+
+// export const selectTaskCounter = state => {
+//   const tasks = selectTasks(state);
+//   console.log('object');
+//   return tasks.reduce(
+//     (acc, task) => {
+//       if (task.completed) {
+//         acc.completed += 1;
+//       } else {
+//         acc.active += 1;
+//       }
+//       return acc;
+//     },
+//     { active: 0, completed: 0 }
+//   );
+// };
+
+export const selectTaskCounter = createSelector([selectTasks], tasks => {
+  return tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+});
